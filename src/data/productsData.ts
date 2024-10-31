03482d70cfdd80a642553b32e812aadf4284fba6
Karimov Dinyar <mr.dinyar@mail.ru>
2024-10-31 23:52:12 +0500

    first commit

---
+++ src/data/productsData.ts
@@ -0,0 +1,28 @@
+import { IProduct } from "../models"
+
+export let products: IProduct[] = [
+    {
+        id:1,
+        title: 'first', 
+        descr: 'some', 
+        price: 1200
+    },
+    {
+        id:2,
+        title: 'second', 
+        descr: 'TEST', 
+        price: 2348
+    },
+    {
+        id:3,
+        title: 'asdfsfda', 
+        descr: 'description', 
+        price: 5446
+    },
+    {
+        id:4,
+        title: 'qqq', 
+        descr: 'deasdfsda', 
+        price: 78678
+    }
+]
