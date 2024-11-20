01916275cd09a23ca314f9241257e17e52a005e1
D1n8 <mr.dinyar@mail.ru>
2024-11-20 13:21:46 +0500

    update CHANGELOG

--- arriba/src/data/categoriesData.ts
+++
@@ -1,8 +0,0 @@
-import { ICategory } from "../models";
-
-export const categories: ICategory[] = [
-    { id: 1, name: 'Телефоны'},
-    { id: 2, name: 'Еда'},
-    { id: 3, name: 'Спорт'},
-    { id: 4, name: 'Одежда'}
-]
